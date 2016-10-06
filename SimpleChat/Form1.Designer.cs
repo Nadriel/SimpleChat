9bbbce765929415f04cb9e01c86ac9ffc07c49b1
Nadriel <kordonis@fournarakis.local>
2016-10-06 11:02:19 +0300

    file1

---
+++ SimpleChat/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SimpleChat
+{
+    partial class GeekChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeekChat));
+            this.groupBoxClient1 = new System.Windows.Forms.GroupBox();
+            this.localPortLabel = new System.Windows.Forms.Label();
+            this.localIpLabel = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIP = new System.Windows.Forms.TextBox();
+            this.groupBoxClient2 = new System.Windows.Forms.GroupBox();
+            this.remotePortLabel = new System.Windows.Forms.Label();
+            this.remoteIpLabel = new System.Windows.Forms.Label();
+            this.textRemotePort = new System.Windows.Forms.TextBox();
+            this.textRemoteIP = new System.Windows.Forms.TextBox();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.messageListBox = new System.Windows.Forms.ListBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.attacth = new System.Windows.Forms.Button();
+            this.groupBoxClient1.SuspendLayout();
+            this.groupBoxClient2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxClient1
+            // 
+            this.groupBoxClient1.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.groupBoxClient1, "groupBoxClient1");
+            this.groupBoxClient1.Controls.Add(this.localPortLabel);
+            this.groupBoxClient1.Controls.Add(this.localIpLabel);
+            this.groupBoxClient1.Controls.Add(this.textLocalPort);
+            this.groupBoxClient1.Controls.Add(this.textLocalIP);
+            this.groupBoxClient1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBoxClient1.Name = "groupBoxClient1";
+            this.groupBoxClient1.TabStop = false;
+            this.groupBoxClient1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // localPortLabel
+            // 
+            resources.ApplyResources(this.localPortLabel, "localPortLabel");
+            this.localPortLabel.Name = "localPortLabel";
+            // 
+            // localIpLabel
+            // 
+            resources.ApplyResources(this.localIpLabel, "localIpLabel");
+            this.localIpLabel.Name = "localIpLabel";
+            // 
+            // textLocalPort
+            // 
+            resources.ApplyResources(this.textLocalPort, "textLocalPort");
+            this.textLocalPort.Name = "textLocalPort";
+            // 
+            // textLocalIP
+            // 
+            resources.ApplyResources(this.textLocalIP, "textLocalIP");
+            this.textLocalIP.Name = "textLocalIP";
+            // 
+            // groupBoxClient2
+            // 
+            this.groupBoxClient2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            resources.ApplyResources(this.groupBoxClient2, "groupBoxClient2");
+            this.groupBoxClient2.Controls.Add(this.remotePortLabel);
+            this.groupBoxClient2.Controls.Add(this.remoteIpLabel);
+            this.groupBoxClient2.Controls.Add(this.textRemotePort);
+            this.groupBoxClient2.Controls.Add(this.textRemoteIP);
+            this.groupBoxClient2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBoxClient2.Name = "groupBoxClient2";
+            this.groupBoxClient2.TabStop = false;
+            // 
+            // remotePortLabel
+            // 
+            resources.ApplyResources(this.remotePortLabel, "remotePortLabel");
+            this.remotePortLabel.Name = "remotePortLabel";
+            // 
+            // remoteIpLabel
+            // 
+            resources.ApplyResources(this.remoteIpLabel, "remoteIpLabel");
+            this.remoteIpLabel.Name = "remoteIpLabel";
+            // 
+            // textRemotePort
+            // 
+            resources.ApplyResources(this.textRemotePort, "textRemotePort");
+            this.textRemotePort.Name = "textRemotePort";
+            // 
+            // textRemoteIP
+            // 
+            resources.ApplyResources(this.textRemoteIP, "textRemoteIP");
+            this.textRemoteIP.Name = "textRemoteIP";
+            // 
+            // textBoxSend
+            // 
+            resources.ApplyResources(this.textBoxSend, "textBoxSend");
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.TextChanged += new System.EventHandler(this.textBoxSend_TextChanged);
+            this.textBoxSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSend_KeyDown);
+            // 
+            // messageListBox
+            // 
+            this.messageListBox.BackColor = System.Drawing.Color.GhostWhite;
+            resources.ApplyResources(this.messageListBox, "messageListBox");
+            this.messageListBox.FormattingEnabled = true;
+            this.messageListBox.Name = "messageListBox";
+            // 
+            // buttonConnect
+            // 
+            resources.ApplyResources(this.buttonConnect, "buttonConnect");
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // sendButton
+            // 
+            resources.ApplyResources(this.sendButton, "sendButton");
+            this.sendButton.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.sendButton.Name = "sendButton";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // attacth
+            // 
+            resources.ApplyResources(this.attacth, "attacth");
+            this.attacth.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.attacth.Name = "attacth";
+            this.attacth.UseVisualStyleBackColor = true;
+            this.attacth.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // GeekChat
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.attacth);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.messageListBox);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.groupBoxClient2);
+            this.Controls.Add(this.groupBoxClient1);
+            this.Name = "GeekChat";
+            this.groupBoxClient1.ResumeLayout(false);
+            this.groupBoxClient1.PerformLayout();
+            this.groupBoxClient2.ResumeLayout(false);
+            this.groupBoxClient2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxClient1;
+        private System.Windows.Forms.Label localPortLabel;
+        private System.Windows.Forms.Label localIpLabel;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIP;
+        private System.Windows.Forms.GroupBox groupBoxClient2;
+        private System.Windows.Forms.Label remotePortLabel;
+        private System.Windows.Forms.Label remoteIpLabel;
+        private System.Windows.Forms.TextBox textRemotePort;
+        private System.Windows.Forms.TextBox textRemoteIP;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.ListBox messageListBox;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button attacth;
+    }
+}
+
---
+++ SimpleChat/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Imported Name Spaces: will be used to connect to each other
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SimpleChat
+{
+    public partial class GeekChat : Form
+    {
+        //declaring variables for use in connectivity
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        public GeekChat()
+        {
+            InitializeComponent();
+            //
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            //we set the local Ip in the Local IP text box
+            textLocalIP.Text = getLocalIP();
+            textRemoteIP.Text = getLocalIP();
+
+        }
+
+        //method that returns Local(our) IP 
+        private string getLocalIP() {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList) {
+
+                if (ip.AddressFamily == AddressFamily.InterNetwork) {
+
+                    return ip.ToString();
+                }
+
+            }
+
+            //in case we don't get an ip from the if clause we give him another home ip
+            return "127.0.0.1";
+        }
+
+
+
+        //method that message callback
+        private void messageCallback(IAsyncResult aResult)
+        {
+            //trying to receive data and converting them to strings
+            try
+            {   
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+
+                    receivedData = (byte[])aResult.AsyncState;
+
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+
+
+                    //print messages to the screen
+                    messageListBox.Items.Add("Friend: " + receivedMessage);
+                    messageListBox.SelectedIndex = messageListBox.Items.Count - 1;
+                    messageListBox.SelectedIndex = -1;
+
+                }
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length,SocketFlags.None, ref epRemote, new AsyncCallback(messageCallback), buffer);
+
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+
+
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get and parse local ip and port
+                epLocal = new IPEndPoint(IPAddress.Parse(textLocalIP.Text), Convert.ToInt32(textLocalPort.Text));
+                sck.Bind(epLocal);
+                
+                //get and parse remote ip and port
+                epRemote = new IPEndPoint(IPAddress.Parse(textRemoteIP.Text), Convert.ToInt32(textRemotePort.Text));
+                sck.Connect(epRemote);
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length,SocketFlags.None, ref epRemote, new AsyncCallback(messageCallback), buffer);
+
+                buttonConnect.Enabled = false;
+                buttonConnect.Text = "Connected";
+                sendButton.Enabled = true;
+                textBoxSend.Focus();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //send text by pressing the enter key -- actually trigger the sendButtonClick method
+        private void textBoxSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //MessageBox.Show("enter key pressed"); used to see if it gets into the if clause
+                sendButton_Click(this, new EventArgs());
+            }
+        }
+
+        private void textBoxSend_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get text from textbox and convert it into bytes and send it
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textBoxSend.Text);
+
+                sck.Send(msg);
+                messageListBox.Items.Add("Me: " + textBoxSend.Text);
+                textBoxSend.Clear();
+                messageListBox.SelectedIndex = messageListBox.Items.Count - 1;
+                messageListBox.SelectedIndex = -1;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
